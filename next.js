4ef650d08bfc6c4b318464f41dab59541417abbf
codercode26 <111521024+codercode26@users.noreply.github.com>
2022-09-24 02:19:10 +0530

    Update next.js

--- next.js
+++ next.js
@@ -50,7 +50,7 @@ function checkAnswer(currentLevel) {
         $("body").removeClass("game-over");
       }, 200);
 
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("Game Over!!");
 
       //2. Call startOver() if the user gets the sequence wrong.
       startOver();
